f53d45d944aeac945d0ecccb80f18609abe8d4c3
Gnadig Peter <xlurzx@gmail.com>
2021-03-18 14:50:22 +0100

    myChart LineChart
    bar chart helyett line chart

--- data/myChartScripts.js
+++ data/myChartScripts.js
@@ -35,7 +35,7 @@ var dataArray = new Array();
 
 var ctx = document.getElementById('myChart');
 var myChart = new Chart(ctx, {
-    type: 'bar',
+    type: 'line',
     data: {
         //labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
         //Labels megkapja a labelArray-t
@@ -46,22 +46,25 @@ var myChart = new Chart(ctx, {
             //data: [12, 19, 3, 5, 2, 3],
             //data megkapja a dataArray-t
             data: dataArray,
+            //borderColor: [rgba(255,99,132,0.2)],
+            
             backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
+                'rgba(255, 255, 255, 0.2)'
+            ],
+            /*  'rgba(255, 99, 132, 0.2)',
                 'rgba(54, 162, 235, 0.2)',
                 'rgba(255, 206, 86, 0.2)',
                 'rgba(75, 192, 192, 0.2)',
                 'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
+                'rgba(255, 159, 64, 0.2)'*/
             borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
+                'rgba(255, 99, 132, 1)'
+            ],
+            /*  ,'rgba(54, 162, 235, 1)',
                 'rgba(255, 206, 86, 1)',
                 'rgba(75, 192, 192, 1)',
                 'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
+                'rgba(255, 159, 64, 1)'*/
             borderWidth: 1
         }]
     },
@@ -142,7 +145,7 @@ var getJSON = function(url) {
       });
   };
   //http://127.0.0.1/data
-  getJSON("data").then(function(data) {
+  getJSON("data2").then(function(data) {
       myJsonDatas = data;
       console.log(myJsonDatas);
       removeAllData(myChart);
